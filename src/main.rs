5dee057347b9db861ac11f2e9a197b24e505aa5f
Antoine Papyn <antoine.papyn@devoteam.com>
2026-07-01 15:45:47 +0200

    create initial server class

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080");
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Server {
+        Server { addr }
+    }
+
+    fn run(&self) {
+        println!("Server is running on port {}...", self.addr);
+    }
 }
