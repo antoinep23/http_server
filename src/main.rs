d973094b817c6f03b26badc124a7808549b20af3
Antoine Papyn <antoine.papyn@devoteam.com>
2026-07-04 17:15:56 +0200

    define request and method

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080");
+    let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
@@ -13,6 +13,24 @@ impl Server {
     }
 
     fn run(&self) {
-        println!("Server is running on port {}...", self.addr);
+        println!("Listening on {}...", self.addr);
     }
+}
+
+struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
 }
